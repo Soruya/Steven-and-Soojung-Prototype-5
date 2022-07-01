2507ce8c9ca0981e43cb3ed7b084862d30fbc047
Soruya <soruyamageofmemories@gmail.com>
2022-06-30 22:27:34 -0700

    Player can move and items have probability

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D playerRB;
+
+    public float playerSpeed;
+    Vector2 playerMovement;
+
+    bool hasItem;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasItem = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerMovement.x = Input.GetAxisRaw("Horizontal");
+        playerMovement.y = Input.GetAxisRaw("Vertical");
     }
 
-    //Movement
+    private void FixedUpdate()
+    {
+        playerRB.MovePosition(playerRB.position + playerMovement * playerSpeed * Time.deltaTime);
+    }
+
+    //Keyboard controls
+    void Interact()
+    {
+        // Pick up item
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
 }
--- Assets/Scripts/StealthItem.cs
+++ Assets/Scripts/StealthItem.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class StealthItem : MonoBehaviour
 {
+    int effectIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,48 @@ public class StealthItem : MonoBehaviour
         
     }
 
-    //Loud noise
-
     //Make player transparent and "invisible"
+    void Invisibility()
+    {
+        Debug.Log("Invisible! " + effectIndex);
+    }
 
     //Increase player speed
+    void SpeedUp()
+    {
+        Debug.Log("Speed up " + effectIndex);
+    }
+
+    //Loud noise
+    void MakeNoise()
+    {
+        Debug.Log("Noise " + effectIndex);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            EffectActivate();
+            Destroy(this.gameObject);
+        }
+    }
+
+    void EffectActivate()
+    {
+        effectIndex = Random.Range(1, 101);
+
+        if (effectIndex > 55)
+        {
+            Invisibility();
+        }
+        if (effectIndex <= 55 && effectIndex > 20)
+        {
+            SpeedUp();
+        }
+        if (effectIndex >= 1 && effectIndex <= 20)
+        {
+            MakeNoise();
+        }
+    }
 }
